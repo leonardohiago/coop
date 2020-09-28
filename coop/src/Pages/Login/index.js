9236707e6cdb5d4896a7732c215a966ff01735ec
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2020-09-27 23:08:04 -0300

    Página de Login com Styled-Components

--- coop/src/Pages/Login/index.js
+++ coop/src/Pages/Login/index.js
@@ -1,136 +1,61 @@
 import React from 'react';
 
+import { Container } from './styles';
+import Button from '../../Components/Button';
+
 import logo from '../../assets/coop-logo.png';
 
 const Login = () => {
   return (
-    <main style={{alignItems: 'center',
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  fontSize: '10px'}}>
+    <Container>
 
-      <aside style={{alignItems: 'center',
-                    backgroundColor: 'var(--roxo)', display: 'flex', height: '100vh', justifyContent: 'center', width: '57vw'}}>
-
-        <p style={{color: '#FFF',
-                          fontFamily: 'Roboto', fontSize: '4.8em', fontStyle: 'normal', fontWeight: 700, lineHeight: '56px', maxWidth: '38vw', padding: '1em 0', textAlign: 'right'}}>
-          Colabore com o <br />trabalho de ONGs
+      <aside>
+        <p>
+          Colabore com o <br/>trabalho de ONGs
         </p>
       </aside>
 
-      <section style={{display: 'flex',
-                      flexDirection: 'column',
-                      height: '100vh',
-                      justifyContent: 'space-between',
-                      padding: '2em 4.6em',
-                      width: '43vw'}}>
+      <section>
 
-        <img src={logo} alt="" style={{
-                      height: '5.4em',
-                      width: '13.2em'}} />
-
+        <img src={logo} alt="" />
         
-        <form style={{display: 'flex',
-                      flexDirection: 'column',
-                      height: '50vh',
-                      justifyContent: 'space-between',
-                      width: '354px'}}>
-          <p style={{color: '#1C1C1C',
-                    fontFamily: 'Roboto',
-                    fontSize: '3em',
-                    fontStyle: 'normal',
-                    fontWeight: '700',
-                    lineHeight: '35px',
-                    textAlign: 'left'}}>
+        <form>
+          <p>
             Login
           </p>
           
-          <div style={{display: 'flex',
-                       flexDirection: 'column'}}>
-            <label htmlFor="email" style={{color: '#1C1C1C',
-                    fontFamily: 'Roboto',
-                    fontSize: '1.8em',
-                    fontStyle: 'normal',
-                    fontWeight: '400',
-                    lineHeight: '21.09px',
-                    marginBottom: '.6em'}}>          
+          <div>
+            <label htmlFor="email">          
               E-mail
             </label>
 
-            <input type="text" name="email" id="email" style={{backgroundColor: '#ECECEC',
-              border: 'none',
-              borderRadius: '1px',
-              height: '46px',
-              width: '354px'
-              }} />
+            <input type="text" name="email" id="email" />
           </div>
           
-          <div style={{display: 'flex',
-                       flexDirection: 'column'}}>
-            <label htmlFor="senha" style={{color: '#1C1C1C',
-                    display: 'flex',
-                    fontFamily: 'Roboto',
-                    fontSize: '1.8em',
-                    fontStyle: 'normal',
-                    fontWeight: '400',
-                    justifyContent: 'space-between',
-                    lineHeight: '21.09px',
-                    marginBottom: '.6em'}}>
+          <div>
+            <label htmlFor="senha" className="label-senha">
               Senha
-              <span><a href="#" style={{color: '#5B25B1',
-                    fontFamily: 'Roboto',
-                    fontSize: '12px',
-                    fontStyle: 'normal',
-                    fontWeight: '400',
-                    lineHeight: '14,06px'}}>
-              Esqueceu a senha?
+              <span><a href="#">
+                Esqueceu a senha?
               </a></span>
             </label>
           
-            <input type="password" name="senha" id="senha"  style={{backgroundColor: '#ECECEC',
-              border: 'none',
-              borderRadius: '1px',
-              height: '46px',
-              width: '354px'
-              }} />
+            <input type="password" name="senha" id="senha" />
           </div>
 
-          <button style={{backgroundColor: '#18B087',
-                          border: 'none',
-                          borderRadius: '100px',
-                          cursor: 'pointer',
-                          height: '44px',
-                          width: '354px',
-                          }}>
-            <span style={{color: '#FFF',
-                  fontFamily: 'Roboto',
-                  fontSize: '18px',
-                  fontStyle: 'normal',
-                  fontWeight: '700',
-                  lineHeight: '21px'}}>
-              Acessar
-            </span>
-          </button>
+          <Button background="var(--verde)" backgroundHover="var(--roxo)">
+            Acessar
+          </Button>
         </form>
 
-        <p style={{color: '#1C1C1C',
-                  fontFamily: 'Roboto',
-                  fontSize: '12px',
-                  fontStyle: 'normal',
-                  fontWeight: '400',
-                  lineHeight: '14,06px'}}>
+        <p>
           Não possui cadastro?
-          <span><a href="#" style={{color: '#5B25B1',
-                  fontFamily: 'Roboto',
-                  fontSize: '12px',
-                  fontStyle: 'normal',
-                  fontWeight: '400',
-                  lineHeight: '14,06px'}}>
-            {' '}Crie sua conta agora
-            </a></span>
+          <span><a href="#">
+            Crie sua conta agora
+          </a></span>
         </p>
       </section>
-    </main>
+    </Container>
   );
 };
 
--- coop/src/Pages/Login/styles.js
+++ coop/src/Pages/Login/styles.js
@@ -1,18 +1,111 @@
 import styled from 'styled-components';
 
-export const Main = styled.div`
-  alignItems: 'center',
-  display: 'flex',
-  flexWrap: 'wrap',
-  fontSize: '10px',
-`;
+export const Container = styled.main`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 10px;
 
-export const Aside = styled.div`
-  alignItems: 'center',
-  backgroundColor: 'var(--roxo)',
-  display: 'flex',
-  height: '100vh',
-  justifyContent: 'center',
-  width: '57vw',
-`;
+  aside {
+    align-items: center;
+    background-color: var(--roxo);
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    width: 57vw;
 
+    p {
+      color: #FFF;
+      font-size: 4.8em;
+      font-weight: 700;
+      line-height: 56px;
+      max-width: 38vw;
+      padding: 1em 0;
+      text-align: right;
+    }
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: space-between;
+    padding: 2em 4.6em;
+    width: 43vw;
+
+    img {
+      height: 5.4em;
+      width: 13.2em;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      height: 50vh;
+      justify-content: space-between;
+      width: 354px;
+
+      p {
+        color: #var(--preto);
+        font-size: 3em;
+        font-weight: 700;
+        line-height: 35px;
+        text-align: left;
+      }
+
+      div {
+        display: flex;
+        flex-direction: column;
+      
+        label {
+          color: var(--preto);
+          font-size: 1.8em;
+          font-weight: 400;
+          line-height: 21.09px;
+          margin-bottom: 0.6em;
+        }
+
+        .label-senha {
+          display: flex;
+          justify-content: space-between;
+
+          a {
+            color: var(--roxo);
+            font-size: 12px;
+            font-weight: 400;
+            line-height: 14.06px;
+          }
+        }
+
+        input {
+          background-color: var(--cinza);
+          border: none;
+          border-radius: 1px;
+          height: 46px;
+          width: 354px;
+        }
+      }
+
+      button {
+        border-radius: 100px;
+        cursor: pointer;
+        font-size: 18px;
+        height: 44px;
+        line-height: 21px;
+        width: 354px;
+      }
+    }
+
+    p {
+      color: var(--preto);
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 14.06px;
+
+      a {
+        color: var(--roxo);
+        margin-left: 2px;
+      }
+    }
+  }
+`;
