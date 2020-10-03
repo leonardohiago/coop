7ddc41a908359e0fbea446734dde75433a54dba5
Leonardo Hiago <49817833+leonardohiago@users.noreply.github.com>
2020-10-03 01:20:32 -0300

    Ajuste nas rotas.

--- coop/src/App.js
+++ coop/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Home from './Pages/Home';
+
 import CadastroOng from './Pages/CadastroOng';
 
-import GlobalStyle from './styles/global';
+import Ongs from './Pages/Ongs';
+import Login from './Pages/Login';
+import Doacao from './Pages/Doacao';
 
+import DashboardHome from './Pages/DashboardIncio';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
@@ -14,6 +21,10 @@ function App() {
           <Switch >
             <Route path="/" component={Home} exact />
             <Route path="/cadastrar-ong" component={CadastroOng} exact />
+            <Route path="/login" component={Login} />
+            <Route path="/ongs" component={Ongs} />
+            <Route path="/doacao" component={Doacao} />
+            <Route path="/dashboard/" component={DashboardHome} exact />
           </Switch>
       </BrowserRouter>
     </div>
