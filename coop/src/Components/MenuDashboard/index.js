c44f9f602f628293240da912d8b730ce3759237b
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-10-05 14:20:35 -0300

    Corrigido links ativos do menu do dashboard

--- coop/src/Components/MenuDashboard/index.js
+++ coop/src/Components/MenuDashboard/index.js
@@ -5,18 +5,18 @@ import { Container } from './styles';
 
 const MenuDashboard = () => (
   <Container>
-      <NavLink to="/dashboard/" activeClassName="nav__link--active">
-          Início
-      </NavLink>
-      <NavLink to="/dashboard/publicacoes" activeClassName="nav__link--active">
-        Publicações
-      </NavLink>
-      <NavLink to="/dashboard/itens-de-doacoes" activeClassName="nav__link--active">
-        Itens de Doações
-      </NavLink>
-      <NavLink to="/dashboard/dados-da-ong" activeClassName="nav__link--active">
-        Dados da ONG
-      </NavLink>
+    <NavLink to="/dashboard/" exact activeClassName="nav__link--active">
+      Início
+    </NavLink>
+    <NavLink to="/dashboard/publicacoes" activeClassName="nav__link--active">
+      Publicações
+    </NavLink>
+    <NavLink to="/dashboard/itens-de-doacoes" activeClassName="nav__link--active">
+      Itens de Doações
+    </NavLink>
+    <NavLink to="/dashboard/dados-da-ong" activeClassName="nav__link--active">
+      Dados da ONG
+    </NavLink>
   </Container>
 );
 
