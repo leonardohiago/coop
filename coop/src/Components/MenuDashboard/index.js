dc9ba9fc6887ad6faa4333098d383f71cc154f40
Samuel Damasceno Filho <samuelfilho@outlook.com>
2020-10-13 13:54:15 -0300

    Adicionado botao para mostrar/esconder menu do dashboard

--- coop/src/Components/MenuDashboard/index.js
+++ coop/src/Components/MenuDashboard/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, ButtonToggleMenu } from './styles';
 
-const MenuDashboard = () => (
-  <Container>
-    <NavLink to="/dashboard/" exact activeClassName="nav__link--active">
-      Início
-    </NavLink>
-    <NavLink to="/dashboard/publicacoes" activeClassName="nav__link--active">
-      Publicações
-    </NavLink>
-    <NavLink to="/dashboard/itens-de-doacoes" activeClassName="nav__link--active">
-      Itens de Doações
-    </NavLink>
-    <NavLink to="/dashboard/dados-da-ong" activeClassName="nav__link--active">
-      Dados da ONG
-    </NavLink>
-  </Container>
-);
+const MenuDashboard = () => {
+  const [toggleMenu, setToggleMenu] = useState(true);
+
+  return (
+    <Container showMenu={toggleMenu}>
+      <ButtonToggleMenu onClick={() => setToggleMenu(!toggleMenu)}>
+        {toggleMenu ? 
+          (<MdChevronLeft size={24} />) : 
+          (<MdChevronRight size={24} />)
+        }
+      </ButtonToggleMenu>
+
+      <NavLink to="/dashboard/" exact activeClassName="nav__link--active">
+        Início
+      </NavLink>
+      <NavLink to="/dashboard/publicacoes" activeClassName="nav__link--active">
+        Publicações
+      </NavLink>
+      <NavLink to="/dashboard/itens-de-doacoes" activeClassName="nav__link--active">
+        Itens de Doações
+      </NavLink>
+      <NavLink to="/dashboard/dados-da-ong" activeClassName="nav__link--active">
+        Dados da ONG
+      </NavLink>
+    </Container>
+  );
+}
 
 export default MenuDashboard;
--- coop/src/Components/MenuDashboard/styles.js
+++ coop/src/Components/MenuDashboard/styles.js
@@ -1,21 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 280px;
+  width: ${props => props.showMenu ? '204px' : '24px'};
   min-height: calc(100vh - 174px);
-  padding-top: 48px;
+  padding-top: 0px;
   background: var(--cinza-claro);
   display: flex;
   flex-direction: column;
+  align-items: center;
+  transition: all 0.5s;
 
   a {
+    width: 100%;
     padding: 12px 0;
-    font-size: 20px;
+    font-size: 16px;
     font-weight: 700;
     color: var(--preto);
     text-decoration: none;
-    text-indent: 56px;
+    text-indent: 24px;
     transition: all 0.3s;
+    white-space: nowrap;
+
+    opacity: ${props => props.showMenu ? '1' : '0'};
 
     &:hover {
       background: var(--cinza);
@@ -25,5 +31,30 @@ export const Container = styled.div`
       color: #fff;
       background: var(--roxo);
     }
+  }
+`;
+
+export const ButtonToggleMenu = styled.button`
+  width: 32px;
+  height: 32px;
+  margin: 32px 0;
+  transform: ${props => props.showMenu ? 'translateX(0)' : 'translateX(0px)'};
+  border: none;
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s;
+  
+  &:hover {
+    background: var(--roxo);
+
+    svg {
+      fill: #fff;
+    }
+  }
+
+  svg {
+    transition: all 0.3s;
   }
 `;
