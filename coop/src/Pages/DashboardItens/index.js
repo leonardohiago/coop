eca0517c8a5237dcfb0e6264c7c3e08564df4c65
Rayanne <rayanne_lima2010@hotmail.com>
2020-10-31 12:53:30 -0300

    Correções - Dashboard Itens

--- coop/src/Pages/DashboardItens/index.js
+++ coop/src/Pages/DashboardItens/index.js
@@ -1,83 +1,131 @@
 import React from "react";
 
-import { Container, Content } from "./styles";
+import { Container, Content, StatusDoacoes, BoxStatus, Form, Subtitle, FieldGroup, ItemsGrid, FieldButtons, SubmitButton, CancelButton } from "./styles";
+import { FaMoneyBillWave, FaTshirt, FaChair, FaHandsHelping, FaBook, FaShower } from "react-icons/fa";
+import { ImSpoonKnife } from "react-icons/im";
+import { GiDogBowl, GiVacuumCleaner } from "react-icons/gi";
+import { MdComputer } from "react-icons/md";
+import { IoIosConstruct } from "react-icons/io";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import MenuDashboard from "../../Components/MenuDashboard";
-import Button from "../../Components/Button";
 
-const DashboardItens = () => (
-  <>
-    <Header logado={true} />
+const DashboardItens = () => {
+  const doacoes = [
+    {
+      id: 1,
+      icone: <FaMoneyBillWave color="#fff" size={48} />,
+      nome: "Dinheiro",
+    },
+    {
+      id: 2,
+      icone: <ImSpoonKnife color="#fff" size={48} />,
+      nome: "Alimento",
+    },
+    {
+      id: 3,
+      icone: <GiDogBowl color="#fff" size={48} />,
+      nome: "Ração",
+    },
+    {
+      id: 4,
+      icone: <MdComputer color="#fff" size={48} />,
+      nome: "Eletrônicos",
+    },
+    {
+      id: 5,
+      icone: <FaTshirt color="#fff" size={48} />,
+      nome: "Roupa",
+    },
+    {
+      id: 6,
+      icone: <FaChair color="#fff" size={48} />,
+      nome: "Móveis",
+    },
+    {
+      id: 7,
+      icone: <FaHandsHelping color="#fff" size={48} />,
+      nome: "Mão de Obra",
+    },
+    {
+      id: 8,
+      icone: <FaBook color="#fff" size={48} />,
+      nome: "Material Escolar",
+    },
+    {
+      id: 9,
+      icone: <GiVacuumCleaner color="#fff" size={48} />,
+      nome: "Material de Limpeza",
+    },
+    {
+      id: 10,
+      icone: <IoIosConstruct color="#fff" size={48} />,
+      nome: "Material de Construção",
+    },
+    {
+      id: 11,
+      icone: <FaShower color="#fff" size={48} />,
+      nome: "Material de Higiene",
+    },
+    {
+      id: 12,
+      icone: <HiOutlineDotsHorizontal color="#fff" size={48} />,
+      nome: "Outro",
+    },
+  ];
 
-    <Container>
-      <MenuDashboard />
+  return (
+    <>
+      <Header logado={true} />
 
-      <Content>
-        <section className="contagem">
-          <div className="dados">
-            <p className="cor-recebidas" id="estilizacao">17</p>
-            <p id="estilizacao_texto">
-              Doações Recebidas
-            </p>
-          </div>
+      <Container>
+        <MenuDashboard />
 
-          <div className="dados">
-        <p className="cor-pendentes" id="estilizacao">42</p>
-        <p id="estilizacao_texto">Doações Pendentes</p>
-      </div>
+        <Content>
+          <StatusDoacoes>
+            <BoxStatus color="verde">
+              <span>17</span>
+              <p>Doações Recebidas</p>
+            </BoxStatus>
 
-      <div className="dados">
-        <p className="cor-canceladas" id="estilizacao">12</p>
-        <p id="estilizacao_texto">Doações Canceladas</p>
-      </div>
+            <BoxStatus color="amarelo">
+              <span>42</span>
+              <p>Doações Pendentes</p>
+            </BoxStatus>
 
+            <BoxStatus color="vermelho">
+              <span>12</span>
+              <p>Doações Canceladas</p>
+            </BoxStatus>
+          </StatusDoacoes>
 
-      </section>
-      
-      <section>
-      <div class="texto-principal">Editar Itens de Doação</div>
-        </section>
+          <Form>
+            <Subtitle>Editar Itens de Doação</Subtitle>
 
-        <section class="itens">
-      <div id="especial" class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div id="especial" class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div id="especial" class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div class="quadrado"></div>
-      <div id="especial" class="quadrado"></div>
-    </section>
+            <FieldGroup>
+              <ItemsGrid>
+                {doacoes.map(({ id, nome, icone }) => (
+                  <li key={id}>
+                    <p>{icone}</p>
+                    <span>{nome}</span>
+                  </li>
+                ))}
+              </ItemsGrid>
+            </FieldGroup>
 
-    <section>
-    <div className="botoes">
-            <Button
-              className="botao-publicar"
-              background="var(--verde)"
-              backgroundHover="var(--roxo)"
-            >
-              Publicar
-            </Button>
-            <Button
-              className="botao-cancelar"
-              background="var(--cinza)"
-              backgroundHover="var(--cinza-claro)"
-            >
-              Cancelar
-            </Button>
-          </div>
-          </section>
-      </Content>
-    </Container>
+            <FieldButtons>
+              <SubmitButton>Atualizar</SubmitButton>
+              <CancelButton>Cancelar</CancelButton>
+            </FieldButtons>
+          </Form>
+        </Content>
+      </Container>
 
-    <Footer />
-  </>
-);
+      <Footer />
+    </>
+  );
+};
 
 export default DashboardItens;
--- coop/src/Pages/DashboardItens/styles.js
+++ coop/src/Pages/DashboardItens/styles.js
@@ -1,105 +1,172 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
   display: flex;
 
-  .contagem {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-
-  .dados {
-    margin: 35px;
-    margin-left: 100px;
-    background: #f9f9f9;
-    border-radius: 25px;
-  }
-
-  .cor-recebidas{
-    color:#18b087;
-  }
-
-  .cor-pendentes{
-    color:#fdd660;
-  }
-
-  .cor-canceladas{
-    color:#fd6060;
-  }
-
-  #estilizacao {
-
-    font-weight: bold;
-    font-size: 64px;
-    margin-top: 46px;
-    margin-left: 12px;
-    margin-right: 96px;
-  }
-
-  #estilizacao_texto {
-
-    font-size: 14px;
-    line-height: 16px;
-    margin-top: -6px;
-    margin-bottom: 22px;
-    margin-right: 50px;
-    margin-left: 19px;
-  }
-
-  .texto-principal {
-   
-    font-weight: bold;
-    font-size: 26px;
-    line-height: 28px;
-    color: #000000;
-    margin-left: 100px;
-    margin-bottom: 30px;
-  }
-
-  .itens {
-    max-width: 930px;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    margin: 0 auto;
-  }
-
-  .quadrado {
-    margin-left: 70px;
-    margin-bottom: 60px;
-    width: 160px;
-    height: 160px;
-    background: #5b25b1;
-    border-radius: 20px;
-  }
-
-  #especial {
-    border: 5px solid #fdd660;
-  }
-
-  .botoes {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .botao-publicar {
-    margin-left: 100px;
-    margin-bottom: 80px;
-    width: 200px;
-    font-size: 17px;
-  }
-
-  .botao-cancelar {
-    margin-left: 60px;
-    margin-bottom: 80px;
-    width: 200px;
-    color: var(--vermelho);
-    font-size: 17px;
-  }
 `;
 
 export const Content = styled.main`
   margin: 0 auto;
+  padding: 32px 48px;
+
+  table {
+    min-width: 656px;
+    margin-top: 24px;
+  }
+`;
+
+export const StatusDoacoes = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const BoxStatus = styled.div`
+  min-width: 192px;
+  min-height: 152px;
+  margin-bottom: 40px;
+  padding: 28px 20px;
+  border-radius: 25px;
+  background: var(--cinza-claro);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  transition: all 0.3s;
+  cursor: pointer;
+
+  & + & {
+    margin-left: 40px;
+  }
+
+  &:hover {
+    box-shadow: 0 0 16px rgba(0, 0, 0, 0.15);
+  }
+
+  span {
+    font-weight: bold;
+    font-size: 64px;
+    color: ${props => `var(--${props.color})`};
+    line-height: 1em;
+  }
+
+  p {
+    font-size: 14px;
+    line-height: 1em;
+  }
+`;
+
+export const Form = styled.div`
+  width: 625px;
+  background: #FFF;
+  border-radius: 8px;
+  
+  display: flex;
+  flex-direction: column;
+
+`;
+
+export const Subtitle = styled.h2`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 20px;
+  font-size: 24px;
+`;
+
+
+export const Field = styled.div`
+  flex: 1;
+  
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+`;
+
+export const FieldGroup = styled.div`
+    flex: 1;
+    display: flex;
+`;
+
+export const ItemsGrid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+  list-style: none;
+
+  li {
+    background: #5B25B1;
+    border: 2px solid #f5f5f5;
+    height: 140px;
+    width: 140;
+    border-radius: 20px;
+    padding: 32px 24px 32px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    text-align: center;
+
+    cursor: pointer;
+
+    span {
+      flex: 1;
+      margin-top: 15px;
+    
+      display: flex;
+      align-items: center;
+      color: white;
+      font-size: 12px;
+    }
+    
+    li.selected {
+      background: #E1FAEC;
+      border: 2px solid #34CB79;
+    }
+  }
+`;
+
+export const FieldButtons = styled.div`
+  flex: 1;
+  
+  display: flex;
+  margin: 24px 0;
+  width: 656px;
+`;
+
+export const SubmitButton = styled.button`
+    width: 240px;
+    height: 48px;
+    background: #18B087;
+    border-radius: 30px;
+    color: #FFF;
+    font-weight: bold;
+    font-size: 16px;
+    border: 0;
+    margin-right: 40px;
+    transition: background-color 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      background: #159270;
+    }
+`;
+
+export const CancelButton = styled.button`
+    width: 240px;
+    height: 48px;
+    background: var(--cinza-claro);
+    border-radius: 30px;
+    color: #FD6060;
+    font-weight: bold;
+    font-size: 16px;
+    border: 0;
+    transition: background-color 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      background: #ddd;
+    }
 `;
