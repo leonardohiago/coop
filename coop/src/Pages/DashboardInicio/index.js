5436d3bfa3c494dc92a907c68d635436af43d685
Jussara <jussara.jandrade1993@outlook.com>
2020-11-13 12:45:14 -0300

    Adicionando integração com axios, puxando os dados do banco para tabela da tela Inicio dashboard

--- coop/src/Pages/DashboardInicio/index.js
+++ coop/src/Pages/DashboardInicio/index.js
@@ -1,64 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Container, Content, StatusDoacoes, BoxStatus, ButtonDoado } from './styles';
+import {
+  Container,
+  Content,
+  StatusDoacoes,
+  BoxStatus,
+  ButtonDoado,
+} from "./styles";
 
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import MenuDashboard from "../../Components/MenuDashboard";
 
+import api from "../../services/api";
+
 const DashboardInicio = () => {
-  const listDoacoes = [
-    {
-      id: '001',
-      doador: 'João Pedro Ferreira da Silva',
-      item: 'Fraldas, Alimento',
-    },
-    {
-      id: '002',
-      doador: 'José Pereira Moraes',
-      item: 'Alimento',
-    },
-    {
-      id: '003',
-      doador: 'Arnaldo Coelho',
-      item: 'Água ',
-    },
-    {
-      id: '004',
-      doador: 'Fred Medeiros',
-      item: 'Dinheiro, Brinquedo',
-    },
-    {
-      id: '005',
-      doador: 'Margarete Silva de Oliveira',
-      item: 'Dinheiro',
-    },
-    {
-      id: '006',
-      doador: 'Maria Josefa da Silva',
-      item: 'Brinquedo',
-    },
-    {
-      id: '007',
-      doador: 'Emerson Silva de Oliveira',
-      item: 'Dinnheiro, Alimento',
-    },
-    {
-      id: '008',
-      doador: 'Josefa Lemos',
-      item: 'Água, Alimento',
-    },
-    {
-      id: '009',
-      doador: 'Felipe Reginaldo',
-      item: 'Dinheiro, Alimento',
-    },
-    {
-      id: '010',
-      doador: 'Tarcisio Mendes',
-      item: 'Dinheiro',
-    },
-  ];
+  const [listaDoacoes, SetListaDoacoes] = useState([]);
+
+  useEffect(() => {
+    api.get(`/doacao`).then((response) => {
+      let doacao = [];
+
+      response.data.forEach((doacoes) => {
+        doacao.push(doacoes);
+      });
+
+      SetListaDoacoes(doacao);
+    });
+  });
 
   return (
     <>
@@ -98,11 +67,11 @@ const DashboardInicio = () => {
             </thead>
 
             <tbody>
-              {listDoacoes.map(doacao => (
-                <tr key={doacao.id}>
-                  <td>{doacao.id}</td>
-                  <td>{doacao.doador}</td>
-                  <td>{doacao.item}</td>
+              {listaDoacoes.map((doacao) => (
+                <tr key={doacao.id_doacao}>
+                  <td>{doacao.id_doacao}</td>
+                  <td>{doacao.nomeCompleto}</td>
+                  <td>{doacao.itensDoacao}</td>
                   <td>
                     <ButtonDoado color="verde">Sim</ButtonDoado>
                     <ButtonDoado color="vermelho">Não</ButtonDoado>
@@ -117,6 +86,6 @@ const DashboardInicio = () => {
       <Footer />
     </>
   );
-}
+};
 
 export default DashboardInicio;
